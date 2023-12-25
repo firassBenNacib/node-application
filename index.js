2c6d2746d860018a9d8cb141b4800dbc48a86e24
FirasBenNacib <128628709+firassBenNacib@users.noreply.github.com>
2023-12-25 23:13:33 +0100

    Update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello From Thetips4you" }');
+});
+
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "This is from page will" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "It works!" }');
+    res.send('{ "response": " Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
